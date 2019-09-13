94373bbabe4aec14df7e23de12b6f659486be087
Paulo Roberto <prmorais1302@gmail.com>
2019-09-13 16:18:02 -0300

    Adicionando configuração do graphql

--- src/shared/logging.interceptor.ts
+++ src/shared/logging.interceptor.ts
@@ -12,19 +12,23 @@ import { tap } from 'rxjs/operators';
 export class LoggingInterceptor implements NestInterceptor {
 	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
 		const req = context.switchToHttp().getRequest();
-		const method = req.method;
-		const url = req.url;
-		const now = Date.now();
+		if (req) {
+			const method = req.method;
+			const url = req.url;
+			const now = Date.now();
 
-		return next
-			.handle()
-			.pipe(
-				tap(() =>
-					Logger.log(
-						`${method} ${url} ${Date.now() - now}ms`,
-						context.getClass().name,
+			return next
+				.handle()
+				.pipe(
+					tap(() =>
+						Logger.log(
+							`${method} ${url} ${Date.now() - now}ms`,
+							context.getClass().name,
+						),
 					),
-				),
-			);
+				);
+		} else {
+			return next.handle();
+		}
 	}
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { UserEntity } from './user.entity';
+import { UserResolver } from './user.resolver';
 
 @Module({
 	imports: [TypeOrmModule.forFeature([UserEntity])],
 	controllers: [UserController],
-	providers: [UserService],
+	providers: [UserService, UserResolver],
 })
 export class UserModule {}
